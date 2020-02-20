196a6360937daff1f186815141824241c5f09d68
shintaro-hirose <joeleshin@eis.hokudai.ac.jp>
2020-02-21 07:58:33 +0900

    [fix]#21 ログアウト時のsuccesbar発動をlogoutactionで行うように修正

--- src/redux/actions/uiActions.js
+++ src/redux/actions/uiActions.js
@@ -1,16 +1,6 @@
 import {
-    OPEN_SUCCESSBAR,
     CLOSE_SUCCESSBAR,
-    LOADING_UI
   } from '../types';
-  
-export const openSuccessbar = (message) => (dispatch) => {
-    dispatch({ type: LOADING_UI });
-    dispatch({
-        type: OPEN_SUCCESSBAR,
-        payload: message
-    });
-  };
 export const closeSuccessbar = () => (dispatch) => {
     dispatch({
         type: CLOSE_SUCCESSBAR,
