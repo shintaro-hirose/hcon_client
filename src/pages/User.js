ec702b92bb0886eb8331f01e5f8411fcffc60920
shintaro-hirose <joeleshin@eis.hokudai.ac.jp>
2020-02-21 10:03:03 +0900

    [add]User画面にTabを追加
    上にTabがあったほうがUX的に良さそうなので、右側上部にタブを追加した

--- src/pages/User.js
+++ src/pages/User.js
@@ -3,9 +3,8 @@ import PropTypes from 'prop-types';
 import { makeStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
 import Paper from '@material-ui/core/Paper';
-import List from '@material-ui/core/List';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemText from '@material-ui/core/ListItemText';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
 
 import UserBestTable from '../components/user/UserBestTable';
 import UserChart from '../components/user/UserChart';
@@ -17,16 +16,16 @@ import { connect } from 'react-redux';
 import { getUserResults } from '../redux/actions/userActions';
 
 const useStyles = makeStyles(theme => ({
-  paper: {
-    width:"100%",
-    boxShadow: theme.shadows[5],
-    marginTop: "10px",
+  tab: {
+        boxShadow: theme.shadows[5],
+        marginBottom: "10px"
+
   },
 }));
 
 const  User = (props) => {
   const classes = useStyles();
-  const [active, setActive] = useState(1);
+  const [value, setValue] = React.useState(0);
   const userHandle = props.match.params.userId;
   useEffect(() => {
     props.getUserResults(userHandle);
@@ -34,14 +33,9 @@ const  User = (props) => {
 
   const loading = props.user.loading;
   const userData = props.user.selectedUserData;
-  const handleActiveChange1 = () => {
-    setActive(1);
-  };
-  const handleActiveChange2 = () => {
-    setActive(2);
-  };
-  const handleActiveChange3 = () => {
-    setActive(3);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
   };
 
   return (
@@ -52,25 +46,27 @@ const  User = (props) => {
         <Grid container spacing={1}>
             <Grid item xs={12} sm={4}>
                 <UserProfile userData={userData}/>
-                <Paper className={classes.paper}>
-                  <List component="nav" aria-label="setting selector">
-                      <ListItem button onClick={handleActiveChange1} selected={active === 1}>
-                          <ListItemText primary="単発ベスト5" />
-                      </ListItem>
-                      <ListItem button onClick={handleActiveChange2} selected={active === 2}>
-                          <ListItemText primary="成功率とDNFの原因" />
-                      </ListItem>
-                      <ListItem button onClick={handleActiveChange3} selected={active === 3}>
-                          <ListItemText primary="単発ベストの推移" />
-                      </ListItem>
-                  </List>
-                </Paper>
             </Grid>
             <Grid item xs={12} sm={8}>
-              {active === 1 ? (
+              <Paper className={classes.tab}>
+                <Tabs
+                  value={value}
+                  indicatorColor="primary"
+                  textColor="primary"
+                  onChange={handleChange}
+                  centered
+                  variant="fullWidth"
+                  aria-label="disabled tabs example"
+                >
+                  <Tab label="単発トップ5" />
+                  <Tab label="成功率とDNFの原因"/>
+                  <Tab label="タイムの推移" />
+                </Tabs>
+              </Paper>
+              {value === 0 ? (
                 <UserBestTable userData={userData}/>
               ) : (
-                active === 2 ? (
+                value === 1 ? (
                   <UserStats userData={userData}/>
                   ) : (
                   <UserChart userData={userData} />
