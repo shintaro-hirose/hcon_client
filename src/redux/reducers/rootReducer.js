794622af6a92d90c024f05d184745443f9e1ee2a
shintaro-hirose <shintarotokyojapan@gmail.com>
2022-07-03 18:03:49 +0900

    perf: #175 removed firestore from redux

--- src/redux/reducers/rootReducer.js
+++ src/redux/reducers/rootReducer.js
@@ -1,15 +1,11 @@
 import userReducer from "./userReducer";
 import uiReducer from "./uiReducer";
 import { combineReducers } from "redux";
-import { firestoreReducer } from "redux-firestore";
-// import { firebaseReducer } from 'react-redux-firebase'
 
 const rootReducer = combineReducers({
   user: userReducer,
   // data: dataReducer,
   UI: uiReducer,
-  firestore: firestoreReducer,
-  // firebase: firebaseReducer
 });
 
 export default rootReducer;
