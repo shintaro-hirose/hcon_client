3632db0cb037e247a0529ddb6326dae6e81682d8
shintaro-hirose <shintarotokyojapan@gmail.com>
2022-07-29 00:51:28 +0900

    feat: Add exhibition field Yoga Fire

--- src/components/exibitions/ExhibitionField.js
+++ src/components/exibitions/ExhibitionField.js
@@ -4,30 +4,36 @@ import Paper from "@material-ui/core/Paper";
 
 import Typography from "@material-ui/core/Typography";
 
+import logo from "../../images/yogafireLogo.svg"
+
 const useStyles = makeStyles((theme) => ({
   root: {
     width: "100%",
     marginBottom: 20,
     padding: "20px 0",
     boxShadow: theme.shadows[5],
-    backgroundColor: "#b2ebf2",
+    backgroundColor: "#ffe0b2",
     color: "black",
+    textAlign: "center",
   },
   title: {
     fontSize: 18,
     textAlign: "center",
     marginBottom: 20,
-    padding: "10px",
+    // padding: "10px",
   },
   body: {
     fontSize: 18,
     textAlign: "center",
-    padding: "0 10px",
+    // padding: "0 10px",
   },
   annotation: {
     fontSize: 18,
     textAlign: "center",
-    padding: "0 10px",
+    // padding: "0 10px",
+  },
+  logo: {
+    width: "60%",
   },
 }));
 
@@ -36,21 +42,27 @@ function ExhibitionField(props) {
   const { appState } = props;
   const word = () => {
     switch(appState.status){
-      case "pre":
-        return "を開催します!";
-      case "ongoing":
-        return "開催中!";
-      case "end":
-        return "が終了しました!";
+      // case "pre":
+      //   return "を開催します!";
+      // case "ongoing":
+      //   return "開催中!";
+      // case "end":
+      //   return "が終了しました!";
       default:
         return "";
     }
   }
   return (
-    !appState.status || appState.status === "" || appState.status === "none" ? <></> :
+    appState.status === "pre" || appState.status === "ongoing" || appState.status === "end"
+     ? 
     <Paper className={classes.root}>
+      <img 
+      alt="ExhibitionLogo"
+      src={logo}
+      className={classes.logo}
+      />
       <Typography className={classes.title}>
-        {appState.title + word()}
+        {word()}
       </Typography>
       <Typography className={classes.body}>
         {appState.body}
@@ -58,7 +70,8 @@ function ExhibitionField(props) {
       <Typography className={classes.annotation}>
         {appState.annotation}
       </Typography>
-    </Paper>
+    </Paper> 
+    : <></>
   );
 }
 
