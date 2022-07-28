3fb5ba8f932be410ccb837ca12f1834e5d98f6f4
shintaro-hirose <shintarotokyojapan@gmail.com>
2022-07-28 21:33:00 +0900

    refactor: UserExibitionbadge refactoring

--- src/components/user/UserExibitionBadge.js
+++ src/components/user/UserExibitionBadge.js
@@ -15,8 +15,17 @@ import thirdFont from "../../images/third-font.svg";
 import { connect } from "react-redux";
 import { getUserResults } from "../../redux/actions/userActions";
 
-function UserExibitionBadge(props) {
-  const userData = props.userData;
+function Badge(props) {
+  const {place, backgroundImage, imgalt, imgsrc} = props;
+  let fontsrc = "";
+  if(place === "first"){
+    fontsrc = firstFont;
+  } else if(place === "second"){
+    fontsrc = secondFont;
+  } else if(place === "third"){
+    fontsrc = thirdFont;
+  }
+
   const useStyles = makeStyles((theme) => ({
     paper: {
       width: "100%",
@@ -24,7 +33,7 @@ function UserExibitionBadge(props) {
       boxShadow: theme.shadows[5],
       marginTop: "10px",
       textAlign: "center",
-      backgroundImage: `url(${sakura})`,
+      backgroundImage: `url(${backgroundImage})`,
     },
     logo: {
       width: "100px",
@@ -37,73 +46,46 @@ function UserExibitionBadge(props) {
   }));
 
   const classes = useStyles();
+  
+  return (
+    <Paper className={classes.paper}>
+      <Box display="flex" alignItems="center" padding="0 50px">
+        <Box flexGrow={1}>
+          <img
+            alt={imgalt}
+            src={imgsrc}
+            className={classes.logo}
+          />
+        </Box>
+        <Box flexGrow={1}>
+          <img
+            alt="fontLogo"
+            src={fontsrc}
+            className={classes.fontLogo}
+          />
+        </Box>
+      </Box>
+    </Paper>
+  )
+}
+
+function UserExibitionBadge(props) {
+  const userData = props.userData;
 
   return (
     <Fragment>
-      {userData.breathOfSpring2020Result ? (
-        userData.breathOfSpring2020Result === "first" ? (
-          <Paper className={classes.paper}>
-            <Box display="flex" alignItems="center" padding="0 50px">
-              <Box flexGrow={1}>
-                <img
-                  alt="harukazeLogo"
-                  src={harukazeLogo}
-                  className={classes.logo}
-                />
-              </Box>
-              <Box flexGrow={1}>
-                <img
-                  alt="fontLogo"
-                  src={firstFont}
-                  className={classes.fontLogo}
-                />
-              </Box>
-            </Box>
-          </Paper>
-        ) : userData.breathOfSpring2020Result === "second" ? (
-          <Paper className={classes.paper}>
-            <Box display="flex" alignItems="center" padding="0 50px">
-              <Box flexGrow={1}>
-                <img
-                  alt="harukazeLogo"
-                  src={harukazeLogo}
-                  className={classes.logo}
-                />
-              </Box>
-              <Box flexGrow={1}>
-                <img
-                  alt="fontLogo"
-                  src={secondFont}
-                  className={classes.fontLogo}
-                />
-              </Box>
-            </Box>
-          </Paper>
-        ) : userData.breathOfSpring2020Result === "third" ? (
-          <Paper className={classes.paper}>
-            <Box display="flex" alignItems="center" padding="0 50px">
-              <Box flexGrow={1}>
-                <img
-                  alt="harukazeLogo"
-                  src={harukazeLogo}
-                  className={classes.logo}
-                />
-              </Box>
-              <Box flexGrow={1}>
-                <img
-                  alt="fontLogo"
-                  src={thirdFont}
-                  className={classes.fontLogo}
-                />
-              </Box>
-            </Box>
-          </Paper>
+      {
+        userData.breathOfSpring2020Result ? (
+          <Badge 
+          place={userData.breathOfSpring2020Result} 
+          backgroundImage={sakura} 
+          imgalt="harukazeLogo" 
+          imgsrc={harukazeLogo} 
+          />
         ) : (
-          <p></p>
+          <></>
         )
-      ) : (
-        <p></p>
-      )}
+      }
     </Fragment>
   );
 }
